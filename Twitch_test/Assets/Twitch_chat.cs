56642af2a652919dd0f3d4bac52c0db0546a111c
Yu Qiao <43658645+yqiao5@users.noreply.github.com>
2019-04-20 21:21:23 -0700

    TwitchGame

---
+++ Twitch_test/Assets/Twitch_chat.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.ComponentModel;
+using System.Net.Sockets;
+using System.IO;
+using UnityEngine.UI;
+
+
+public class Twitch_chat : MonoBehaviour
+{
+    private TcpClient twitchClient;
+    private StreamReader reader;
+    private StreamWriter writer;
+    // Start is called before the first frame update
+
+    public string username, password, channelName; // Get the password from twitchapps.com
+
+    public Text chatBox;
+
+    void Start()
+    {
+        Connect();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!twitchClient.Connected)
+        {
+            Connect();
+        }
+
+        ReadChat();
+    }
+
+    private void Connect()
+    {
+        twitchClient = new TcpClient("irc.chat.twitch.tv", 6667);
+        reader = new StreamReader(twitchClient.GetStream());
+        writer = new StreamWriter(twitchClient.GetStream());
+
+        writer.WriteLine("PASS " + password);
+        writer.WriteLine("NICK " + username);
+        writer.WriteLine("USER " + username + " 8 * :" + username);
+        writer.WriteLine("JOIN #" + channelName);
+        writer.Flush();
+
+
+
+    }
+
+    private void ReadChat()
+    {
+        if(twitchClient.Available > 0)
+        {
+            var message = reader.ReadLine();
+
+            if (message.Contains("PRIVMSG"))
+            {
+                //get the users name
+                var splitPoint = message.IndexOf("!", 1);
+                var chatName = message.Substring(0, splitPoint);
+                chatName = chatName.Substring(1);
+
+                //get the users message
+                splitPoint = message.IndexOf(":", 1);
+                message = message.Substring(splitPoint + 1);
+                print(String.Format("{0}: {1}", chatName, message));
+
+            }
+
+
+           
+
+
+        }
+
+
+
+    }
+
+}
